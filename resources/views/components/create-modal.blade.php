b637e275762e7e89719bf82c701b5b35113bfc61
engsahaly <engsahaly@gmail.com>
2023-09-02 18:24:37 +0200

    enhancing create modal

---
+++ resources/views/components/create-modal.blade.php
@@ -0,0 +1,26 @@
+<!-- Modal -->
+<div class="modal fade" id="createModal" tabindex="-1" aria-hidden="true" wire:ignore.self>
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel1">{{ $title }}</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form wire:submit.prevent='submit'>
+                <div class="modal-body">
+                    <div class="row">
+                        {{ $slot }}
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">
+                        Close
+                    </button>
+                    <button type="submit" class="btn btn-primary">
+                        @include('admin.loading', ['buttonName' => 'Create'])
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
