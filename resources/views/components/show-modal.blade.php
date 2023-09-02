ea9161f45bc1724adb6f3996c1431d6908f4af0e
engsahaly <engsahaly@gmail.com>
2023-09-02 18:37:15 +0200

    enhancing displaying mdoal

---
+++ resources/views/components/show-modal.blade.php
@@ -0,0 +1,21 @@
+<!-- Modal -->
+<div class="modal fade" id="showModal" tabindex="-1" aria-hidden="true" wire:ignore.self>
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel1">{{ $title }}</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="row">
+                    {{ $slot }}
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">
+                    Close
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
