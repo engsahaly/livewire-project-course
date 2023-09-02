01382db438b04cfb5b6199c8ff10a47912b7ea7f
engsahaly <engsahaly@gmail.com>
2023-09-02 18:06:24 +0200

    enhancing error syntax

---
+++ resources/views/admin/error.blade.php
@@ -0,0 +1,3 @@
+@error($property)
+    <span class="text-danger">{{ $message }}</span>
+@enderror
--- resources/views/admin/skills/skills-create.blade.php
+++ resources/views/admin/skills/skills-create.blade.php
@@ -12,17 +12,13 @@
                         <div class="col-md-6 mb-0">
                             <label class="form-label">Name</label>
                             <input type="text" class="form-control" placeholder="Name" wire:model='name' />
-                            @error('name')
-                                <span class="text-danger">{{ $message }}</span>
-                            @enderror
+                            @include('admin.error', ['property' => 'name'])
                         </div>
                         <div class="col-md-6 mb-0">
                             <label class="form-label">Progress</label>
                             <input type="number" class="form-control" placeholder="10" min="1" max="100"
                                 wire:model='progress' />
-                            @error('progress')
-                                <span class="text-danger">{{ $message }}</span>
-                            @enderror
+                            @include('admin.error', ['property' => 'progress'])
                         </div>
                     </div>
                 </div>
